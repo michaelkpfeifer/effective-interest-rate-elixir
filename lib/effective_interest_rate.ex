ecac6187779af55d032ff44b0fe23a49a0b71f53
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2017-08-10 23:05:44 +0200

    Initial commit

---
+++ lib/effective_interest_rate.ex
@@ -0,0 +1,19 @@
+defmodule EffectiveInterestRate do
+  @moduledoc """
+  EffectiveInterestRate implements the computation of the effective
+  interest rate of a series of payments in a very general case.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EffectiveInterestRate.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/effective_interest_rate_test.exs
@@ -0,0 +1,8 @@
+defmodule EffectiveInterestRateTest do
+  use ExUnit.Case
+  doctest EffectiveInterestRate
+
+  test "greets the world" do
+    assert EffectiveInterestRate.hello() == :world
+  end
+end
