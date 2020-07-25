dc0966ed0dadb3eb7f23e83049ce6bd0a1599f68
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2020-07-25 08:16:26 +0200

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -6,23 +6,20 @@ defmodule EffectiveInterestRate.Mixfile do
       app: :effective_interest_rate,
       version: "0.1.0",
       elixir: "~> 1.5",
-      start_permanent: Mix.env == :prod,
       deps: deps()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, only: :dev},
-      {:earmark, only: :dev}
+      {:ex_doc, only: :dev, runtime: false},
+      {:credo, only: :dev}
     ]
   end
 end
