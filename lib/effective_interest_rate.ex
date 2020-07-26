8e73bd2ea1fce855378ea87aeda9b242457a59ff
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2020-07-26 12:44:21 +0200

    Rewrite EffectiveInterestRate module and tests

--- lib/effective_interest_rate.ex
+++ lib/effective_interest_rate.ex
@@ -1,112 +1,44 @@
 defmodule EffectiveInterestRate do
+  @newton_start_value -0.75
+  @max_iteration_difference 1.0e-9
   @max_iterations 64
-  @max_diff_iterations 10.0e-10
 
   @moduledoc """
-  EffectiveInterestRate implements the computation of the effective
-  interest rate of a series of payments in a very general case.
+
+  The EffectiveInterestRate module implements the computation of the
+  effective interest rate of a stream of payments in a very general
+  case.
+
+  Details about payment streams can be found in the documentation of
+  the `PaymentStream` module.
+
   """
 
-  def effective_interest_rate(payments_with_date) do
-    payments_with_offset = convert_to_payments_with_offset(payments_with_date)
-    payments_with_offset_derivative = Enum.map(payments_with_offset, fn(payment_with_offset) ->
-      derive_payment_with_offset(payment_with_offset)
-    end)
-    effective_interest_rate(payments_with_offset, payments_with_offset_derivative, 0, -0.75)
-  end
+  @doc ~S"""
 
-  defp effective_interest_rate(_, _, @max_iterations, iteration) do
-    iteration
-  end
-
-  defp effective_interest_rate(payments_with_offset,
-    payments_with_offset_derivative,
-    iterations_count,
-    previous_iteration) do
-    nominator = evaluate(payments_with_offset, previous_iteration)
-    denominator = evaluate(payments_with_offset_derivative, previous_iteration)
-    next_iteration = previous_iteration - (nominator / denominator)
-    if abs(next_iteration - previous_iteration) < @max_diff_iterations do
-      next_iteration
-    else
-	effective_interest_rate(payments_with_offset,
-	  payments_with_offset_derivative,
-	  iterations_count + 1,
-	  next_iteration)
-    end
-  end
-
-  @doc """
-  first_payment_with_date returns the oldest payment in a list of
-  payments with date.
+  Computes the effective interest rate of a payment stream
 
   ## Examples
 
-      iex> EffectiveInterestRate.first_payment_with_date([{1000, ~D[2000-01-01]}, {1000, ~D[2001-01-01]}])
-      {1000, ~D[2000-01-01]}
+    ```
+    iex> payments = [{2000, ~D[2021-06-01]}, {-1000, ~D[2022-06-01]}, {-1000, ~D[2023-06-01]}]
+    ...> {:ok, rate} = EffectiveInterestRate.effective_interest_rate(payments)
+    ...> Float.round(rate, 6)
+    0.0
+
+    ```
 
   """
-  def first_payment_with_date([payment_with_date | remaining_payments_with_date]) do
-    first_payment_with_date(payment_with_date, remaining_payments_with_date)
-  end
 
-  defp first_payment_with_date(payment_with_date, []) do
-    payment_with_date
-  end
+  def effective_interest_rate(payment_stream) do
+    relative_payment_stream = PaymentStream.to_relative_payment_stream(payment_stream)
 
-  defp first_payment_with_date(payment_with_date, [next_payment_with_date | next_remaining_payments_with_date]) do
-    {_, date} = payment_with_date
-    {_, next_date} = next_payment_with_date
-    if (date < next_date) do
-      first_payment_with_date(payment_with_date, next_remaining_payments_with_date)
-    else
-      first_payment_with_date(next_payment_with_date, next_remaining_payments_with_date)
-    end
-  end
-
-  def offset_in_year(date) do
-    year = date.year
-    {:ok, jan_first} = Date.new(year, 1, 1)
-    number_of_days = Date.diff(date, jan_first)
-
-    if Date.leap_year?(date) do
-      number_of_days / 366
-    else
-      number_of_days / 365
-    end
-  end
-
-  def payment_offset(payment_with_date, first_payment_year, first_payment_offset) do
-    {_, payment_date} = payment_with_date
-    year = payment_date.year
-    year_difference = year - first_payment_year
-    payment_offset_in_year = offset_in_year(payment_date)
-    payment_offset_in_year + year_difference - first_payment_offset
-  end
-
-  def convert_to_payments_with_offset(payments_with_date) do
-    {_, first_payment_date} = first_payment_with_date(payments_with_date)
-    first_payment_offset = offset_in_year(first_payment_date)
-    first_payment_year = first_payment_date.year
-    Enum.map(payments_with_date, fn(payment_with_date) ->
-      {payment, _} = payment_with_date
-      offset = payment_offset(payment_with_date, first_payment_year, first_payment_offset)
-      {payment, offset}
-    end)
-  end
-
-  def evaluate(terms, x) do
-    List.foldl(terms,
-      0,
-      fn(term, sum) ->
-	{amount, offset} = term
-	sum + amount * :math.pow((1 + x), -offset)
-      end
+    NewtonIteration.iterate(
+      PaymentStream.net_present_value(relative_payment_stream),
+      PaymentStream.net_present_value_derivative(relative_payment_stream),
+      @newton_start_value,
+      @max_iteration_difference,
+      @max_iterations
     )
-  end
-
-  def derive_payment_with_offset(payment_with_offset) do
-    {amount, offset} = payment_with_offset
-    {-offset * amount, -(-offset - 1)}
   end
 end
--- test/effective_interest_rate_test.exs
+++ test/effective_interest_rate_test.exs
@@ -2,114 +2,45 @@ defmodule EffectiveInterestRateTest do
   use ExUnit.Case
   doctest EffectiveInterestRate
 
-  test "first payment of a list of one payments is the only payment in the list" do
-    payments_with_date = [{2000, ~D[2015-06-01]}]
-    first_payment_with_date = EffectiveInterestRate.first_payment_with_date(payments_with_date)
-    assert {2000, ~D[2015-06-01]} == first_payment_with_date
+  test "effective interest rate is 0 in a very simple case" do
+    payments = [{2000, ~D[2013-06-01]}, {-1000, ~D[2014-06-01]}, {-1000, ~D[2015-06-01]}]
+
+    {:ok, effective_interest_rate} = EffectiveInterestRate.effective_interest_rate(payments)
+
+    assert_in_delta(0.0, effective_interest_rate, 1.0e-6)
   end
 
-  test "first payment of a list of payments is the oldest payment in the list" do
-    payments_with_date = [{-1000, ~D[2015-06-01]},
-                          {-1000, ~D[2014-06-01]},
-                          {2000, ~D[2013-06-01]},
-                          {-100, ~D[2015-07-01]}]
-    first_payment_with_date = EffectiveInterestRate.first_payment_with_date(payments_with_date)
-    assert {2000, ~D[2013-06-01]} == first_payment_with_date
-  end
+  test "effective interest rate is correct has the expected sign in a simple case" do
+    payments = [
+      {2000, ~D[2013-06-01]},
+      {-1000, ~D[2014-06-01]},
+      {-1000, ~D[2015-06-01]},
+      {-100, ~D[2015-07-01]}
+    ]
 
-  test "offset in year respects leap year" do
-    assert 0 / 365 == EffectiveInterestRate.offset_in_year(~D[2015-01-01])
-    assert 0 / 366 == EffectiveInterestRate.offset_in_year(~D[2016-01-01])
-    assert 1 / 365 == EffectiveInterestRate.offset_in_year(~D[2015-01-02])
-    assert 1 / 366 == EffectiveInterestRate.offset_in_year(~D[2016-01-02])
-    assert 364 / 365 == EffectiveInterestRate.offset_in_year(~D[2015-12-31])
-    assert 365 / 366 == EffectiveInterestRate.offset_in_year(~D[2016-12-31])
-  end
+    {:ok, effective_interest_rate} = EffectiveInterestRate.effective_interest_rate(payments)
 
-  test "payment offset takes first payment date into consideration" do
-    payment_offset_1 = EffectiveInterestRate.payment_offset({1000, ~D[2015-01-01]}, 2015, 0)
-    assert 0.0 == payment_offset_1
-    payment_offset_2 = EffectiveInterestRate.payment_offset({1000, ~D[2016-01-01]}, 2015, 0)
-    assert 1.0 == payment_offset_2
-    payment_offset_3 = EffectiveInterestRate.payment_offset({1000, ~D[2016-07-02]}, 2016, 0)
-    assert 0.5 == payment_offset_3
-    payment_offset_4 = EffectiveInterestRate.payment_offset({1000, ~D[2016-07-02]}, 2016, 0.5)
-    assert 0.0 == payment_offset_4
-  end
-
-  test "conversion to payments with offsets succeeds in trvial case" do
-    payments_with_date = [{2000, ~D[2015-06-01]}]
-    payments_with_offset = EffectiveInterestRate.convert_to_payments_with_offset(payments_with_date)
-    [{payment, offset}] = payments_with_offset
-    assert 2000 == payment
-    assert 0.0 == offset
-  end
-
-  test "conversion to payments with offsets succeeds in simple case" do
-    payments_with_date = [{2000, ~D[2013-06-01]},
-			  {-1000, ~D[2014-06-01]},
-			  {-1000, ~D[2015-06-01]},
-			  {-100, ~D[2015-07-01]}]
-    payments_with_offset = EffectiveInterestRate.convert_to_payments_with_offset(payments_with_date)
-    payments = Enum.map(payments_with_offset, fn(entry) -> elem(entry, 0) end)
-    [offset1, offset2, offset3, offset4] = Enum.map(payments_with_offset, fn(entry) -> elem(entry, 1) end)
-    assert [2000, -1000, -1000, -100] == payments
-    assert 0.0 == offset1
-    assert 1.0 == offset2
-    assert 2.0 == offset3
-    assert 2.05 < offset4
-    assert 2.1 > offset4
-  end
-
-  test "evaluation of sum expression returns expected sum" do
-    payments_with_date = [{2000, ~D[2013-06-01]},
-			  {-1000, ~D[2014-06-01]},
-			  {-1000, ~D[2015-06-01]}]
-    payments_with_offset = EffectiveInterestRate.convert_to_payments_with_offset(payments_with_date)
-    value0 = EffectiveInterestRate.evaluate(payments_with_offset, 0.0)
-    assert 0.0 == value0
-    value1 = EffectiveInterestRate.evaluate(payments_with_offset, 1.0)
-    assert 1250.0 == value1
-  end
-
-  test "derivative of terms in sum shows expected results" do
-    assert {0.0, 1.0} == EffectiveInterestRate.derive_payment_with_offset({1000.0, 0.0})
-    assert {-1000.0, 2.0} == EffectiveInterestRate.derive_payment_with_offset({1000.0, 1.0})
-    assert {-2000.0, 3.0} == EffectiveInterestRate.derive_payment_with_offset({1000.0, 2.0})
-    assert {-500.0, 1.5} == EffectiveInterestRate.derive_payment_with_offset({1000.0, 0.5})
-  end
-
-  test "effective interest rate is correct for very simple case" do
-    payments_with_date = [{2000, ~D[2013-06-01]},
-  			  {-1000, ~D[2014-06-01]},
-  			  {-1000, ~D[2015-06-01]}]
-    effective_interest_rate = EffectiveInterestRate.effective_interest_rate(payments_with_date)
-    assert abs(effective_interest_rate) < 10.0e-6
-  end
-
-  test "effective interest rate is correct for simple case" do
-    payments_with_date = [{2000, ~D[2013-06-01]},
-  			  {-1000, ~D[2014-06-01]},
-  			  {-1000, ~D[2015-06-01]},
-  			  {-100, ~D[2015-07-01]}]
-    effective_interest_rate = EffectiveInterestRate.effective_interest_rate(payments_with_date)
     assert effective_interest_rate > 0
   end
 
-  test "effective interest rate is correct for series of monthly payments" do
-    payments_with_date = [{240000, ~D[2015-01-01]}] ++
-    for year <- Enum.to_list(2015..2034), month <- Enum.to_list(1..12) do
-      {:ok, date} = Date.new(year, month, 1)
-      {-1200, date}
-    end
-    effective_interest_rate = EffectiveInterestRate.effective_interest_rate(payments_with_date)
-    assert abs(effective_interest_rate - 1.91/100) < 10.0e-4
+  test "effective interest rate returns the expected value for a stream of monthly payments" do
+    payments =
+      [{240_000, ~D[2015-01-01]}] ++
+        for year <- Enum.to_list(2015..2034), month <- Enum.to_list(1..12) do
+          {:ok, date} = Date.new(year, month, 1)
+          {-1200, date}
+        end
+
+    {:ok, effective_interest_rate} = EffectiveInterestRate.effective_interest_rate(payments)
+
+    assert_in_delta(1.91 / 100, effective_interest_rate, 1.0e-3)
   end
 
-  test "effective interest rate is correct for simple real life case" do
-    payments_with_date = [{-1065.25, ~D[2011-04-21]},
-  			  {130.69, ~D[2014-05-23]}]
-    effective_interest_rate = EffectiveInterestRate.effective_interest_rate(payments_with_date)
-    assert abs(effective_interest_rate - (- 0.4931)) < 0.0001
+  test "effective interest rate returns the expected value for simple real life case" do
+    payments = [{-1065.25, ~D[2011-04-21]}, {130.69, ~D[2014-05-23]}]
+
+    {:ok, effective_interest_rate} = EffectiveInterestRate.effective_interest_rate(payments)
+
+    assert_in_delta(-0.4931, effective_interest_rate, 1.0e-3)
   end
 end
